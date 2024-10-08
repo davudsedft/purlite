233507dddc5d6688f1c73bdc0d9f5935137065f4
david <davudseft@gmail.com>
2024-10-09 02:24:54 +0330

    Delete v2ray/src/main/java/com/dabut/lib/v2ray/interfaces/TrafficListener.java

--- v2ray/src/main/java/com/dabut/lib/v2ray/interfaces/TrafficListener.java
+++
@@ -1,7 +0,0 @@
-package com.dabut.lib.v2ray.interfaces;
-
-import android.content.Context;
-
-public interface TrafficListener {
-    void onTrafficChanged(long uploadSpeed, long downloadSpeed, long uploadedTraffic, long downloadedTraffic, Context context);
-}
